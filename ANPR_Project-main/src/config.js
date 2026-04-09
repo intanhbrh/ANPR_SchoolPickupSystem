0eb22650e9f49cb737fdb7044404e44c9049ff79
Intan <154000221+intanhbrh@users.noreply.github.com>
2026-04-09 10:22:31 +0800

    Delete ANPR_Project-main directory

--- ANPR_Project-main/src/config.js
+++
@@ -1,8 +0,0 @@
-// src/config.js
-
-// Ensure this IP and protocol match your Flask-SocketIO setup
-// NOTE: Use your machine's local IP or a hostable address
-export const SOCKET_URL = "http://10.250.34.10:80";
-
-// Correct Passcode
-export const CORRECT_PASSCODE = "HELPIS"; 
